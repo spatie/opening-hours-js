1f9b41a5d415d5cee61bbfe20f19a96e54baef3f
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-10-06 10:34:35 +0200

    Expose Validation to package

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ export { default as OpeningHours } from './OpeningHours';
 export { default as OpeningHoursForDay } from './OpeningHoursForDay';
 export { default as Time } from './Time';
 export { default as TimeRange } from './TimeRange';
+export { default as Validation } from './lib/Validation';
 
 export { default } from './OpeningHours';
